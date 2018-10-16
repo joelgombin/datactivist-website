6c21c01bd6d591a86f34298e8a22488adb184c0c
Peh4 <peha@free.fr>
2018-10-16 12:27:17 +0200

    Better css organisation + respansive fixes

--- hugo/themes/datacthemist/static/js/action.js
+++ hugo/themes/datacthemist/static/js/action.js
@@ -1,6 +1,9 @@
 'use strict';
 
 $(function() {
+  
+  $('body').addClass('borders');
+
   $('.slides').slick({
     autoplay: false,
     prevArrow: $('.prev'),
@@ -35,3 +38,34 @@ $(function() {
     }    
   })
 })
+
+/**!
+Navigation Button Toggle class
+*/
+(function() {
+
+// old browser or not ?
+if ( !('querySelector' in document && 'addEventListener' in window) ) {
+return;
+}
+window.document.documentElement.className += ' js-enabled';
+
+function toggleNav() {
+
+// Define targets by their class or id
+var button = document.querySelector('.nav-button');
+var target = document.querySelector('.main');
+
+// click-touch event
+if ( button ) {
+  button.addEventListener('click',
+  function (e) {
+      button.classList.toggle('is-active');
+    target.classList.toggle('is-opened');
+    e.preventDefault();
+  }, false );
+}
+} // end toggleNav()
+
+toggleNav();
+}());
